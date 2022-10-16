b86cec6e33ceab12a98b4c1317b838ab004f4ae5
Jongbeom-Kwon <bolk9652@naver.com>
2022-10-16 15:46:53 +0900

    add ports

---
+++ example/port/repository.py
@@ -0,0 +1,26 @@
+from typing import Optional, Protocol, TypeVar
+
+A = TypeVar("A")
+I_contra = TypeVar("I_contra", contravariant=True)
+
+
+class CollectionOrientedRepository(Protocol[A, I_contra]):
+    async def add(self, _aggregate: A) -> None:
+        ...
+
+    async def get(self, _id: I_contra) -> Optional[A]:
+        ...
+
+    async def delete(self, _aggregate: A) -> None:
+        ...
+
+
+class PersistenceOrientedRepository(Protocol[A, I_contra]):
+    async def save(self, _aggregate: A) -> None:
+        ...
+
+    async def get(self, _id: I_contra) -> Optional[A]:
+        ...
+
+    async def delete(self, _aggregate: A) -> None:
+        ...
